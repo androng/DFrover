0c7b68d3b839c74cd8a20f344d2946bb8e54bc10
Andrew <i.am.andrew.ong@gmail.com>
2012-04-14 08:50:16 -0700

    Added answers and added blanks in main control file

--- Full_code/control.ino
+++ Full_code/control.ino
@@ -5,53 +5,48 @@ Exercise 1: Basic movement to get familar with functions
 -Move forward and turn left for 2 seconds
 -Move forward and turn right for 2 seconds
 *********/
-//void control(){
-//    /****** Move forward for two seconds ******/
-//    /* Set direction of motor 1 and 2 */
-//    forward(1);
-//    forward(2);
-//    /* Set speed of motor 1 and 2 */
-//    setSpeed(1, 255);
-//    setSpeed(2, 255);
-//    /* Do nothing for 2 seconds. */
-//    delay(2000);
-//    
-//    /******  Move backward for two seconds  ******/
-//    /* Set direction of motor 1 and 2 */
-//    backward(1);
-//    backward(2);
-//    /* Do nothing for 2 seconds. */
-//    delay(2000);
-//    
-//    /******  Move forward and turn left for 2 seconds  ******/
-//    /* Turn both motors off */
-//    setSpeed(1, 0);
-//    setSpeed(2, 0);
-//    /* Set direction of motor 1 forward */
-//    forward(1);
-//    /* Turn motor 1 on (increase speed). */
-//    setSpeed(1, 255);
-//    /* Do nothing for 2 seconds. */
-//    delay(2000);
-//    
-//    /******  Move forward and turn right for 2 seconds  ******/
-//    /* Turn both motors off */
-//    setSpeed(1, 0);
-//    setSpeed(2, 0);
-//    /* Set direction of motor 2 forward */
-//    forward(2);
-//    /* Turn motor 2 on (increase speed). */
-//    setSpeed(2, 255);
-//    /* Do nothing for 2 seconds. */
-//    delay(2000);
-//    
-//    /******  Stop  ******/
-//    /* Turn both motors off */
-//    setSpeed(1, 0);
-//    setSpeed(2, 0);
-//    /* Do nothing for 4 seconds. */
-//    delay(4000);
-//}
+void control(){
+    /****** Move forward for two seconds ******/
+    /* Set direction of motor 1 and 2 */
+    
+    /* Set speed of motor 1 and 2 */
+    
+    /* Do nothing for 2 seconds. */
+    
+    
+    /******  Move backward for two seconds  ******/
+    /* Set direction of motor 1 and 2 */
+    
+    /* Do nothing for 2 seconds. */
+    
+    
+    /******  Move forward and turn left for 2 seconds  ******/
+    /* Turn both motors off */
+    
+    /* Set direction of motor 1 forward */
+    
+    /* Turn motor 1 on (increase speed). */
+    
+    /* Do nothing for 2 seconds. */
+    
+    
+    /******  Move forward and turn right for 2 seconds  ******/
+    /* Turn both motors off */
+    
+    /* Set direction of motor 2 forward */
+    
+    /* Turn motor 2 on (increase speed). */
+    
+    /* Do nothing for 2 seconds. */
+    
+    
+    /******  Stop  ******/
+    /* Turn both motors off */
+    
+    /* Do nothing for 4 seconds. */
+    
+    
+}
 
 /******* 
 Exercise 2: Light-triggered robot. 
@@ -60,31 +55,26 @@ Exercise 2: Light-triggered robot.
 *********/
 //void control(){
 //    /* Turn both motors off */
-//    setSpeed(1, 0);
-//    setSpeed(2, 0);
+//
 //
 //    /******  Moves forward if there is light  ******/
 //    /* If there is more light */
-//    if(moreLight()){
+//    if(           ){
 //        /* Set direction of motor 1 and 2 */
-//        forward(1);
-//        forward(2);
+//
 //        /* Set speed of motor 1 and 2 */
-//        setSpeed(1, 255);
-//        setSpeed(2, 255);
+//
 //    }
 //    /******  Moves backward if light is blocked  ******/
-//    if(lessLight()){
+//    if(            ){
 //        /* Set direction of motor 1 and 2 */
-//        backward(1);
-//        backward(2);
+
 //        /* Set speed of motor 1 and 2 */
-//        setSpeed(1, 255);
-//        setSpeed(2, 255);
+
 //    }
 //    
 //    /* Wait 0.1s */
-//    delay(100);
+
 //}
 
 /******* 
@@ -94,32 +84,31 @@ Exercise 3: Line follower: follows a line between the two sensors.
 *********/
 //void control(){
 //    /* Set direction of motor 1 and 2 */
-//    forward(1);
-//    forward(2);
+//
 //
 //    /******  Turn off motor 1 if line under sensor 1  ******/
 //    /* If sensor 1 is over a line */
-//    if(line(1)){
+//    if(         ){
 //        /* Turn motor 1 off */
-//        setSpeed(1, 0);
+
 //    }
 //    else {
 //        /* Turn motor 1 on */
-//        setSpeed(1, 255);
+//
 //    }
 //    /******  Turn off motor 2 if line under sensor 2  ******/
 //    /* If sensor 2 is over a line */
-//    if(line(2)){
+//    if(         ){
 //        /* Turn motor 2 off */
-//        setSpeed(2, 0);
+//
 //    }
 //    else {
 //        /* Turn motor 2 on */
-//        setSpeed(2, 255);
+//
 //    }
 //    
 //    /* Wait 0.1s */
-//    delay(100);
+//
 //}
 
 
